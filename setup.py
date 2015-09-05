e381bf8a2c4c402677192883834646dc9db9d2e2
Iury O. g. Figueiredo <tau@ocean.ocean.net>
2015-09-05 15:01:41 -0300

    ..

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from distutils.core import setup
 setup(name="untwisted",
-      version="0.1",
+      version="0.0.1",
       packages=["untwisted", 
                 "untwisted.utils"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br")
+
 
 
