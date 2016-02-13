6bb58e639dca83eb958304d25684bb6d28b5de5e
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-02-12 22:34:34 -0200

    Fixing examples.

---
+++ demo/extra/echo_server.py
@@ -0,0 +1,41 @@
+"""
+"""
+
+from untwisted.network import Spin, xmap, core
+from untwisted.iostd import Server, Stdout, Stdin, ACCEPT, LOAD, CLOSE
+
+class EchoServer(object):
+    def __init__(self, spin):
+        xmap(spin, ACCEPT, self.handle_accept)
+
+    def handle_accept(self, server, client):
+        Stdin(client)
+
+        Stdout(client)
+       
+        xmap(client, LOAD, self.handle_load)
+        xmap(client, CLOSE, self.handle_close)
+
+    def handle_load(self, client, data):
+        client.dump(data)
+
+    def handle_close(self, client, err):
+        client.destroy()
+        client.close()
+
+if __name__ == '__main__':
+    # We create a Spin class pretty much as we would
+    # do with a socket class.
+    spin = Spin()
+    spin.bind(('', 1234))
+    spin.listen(200)
+
+
+    Server(spin)
+
+
+    EchoServer(spin)
+    core.gear.mainloop()
+
+
+
