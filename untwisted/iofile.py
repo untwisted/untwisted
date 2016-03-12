d2f4209d3a1b6e145d9f36c6ed60c1738add7dc2
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-03-12 02:59:12 -0400

    Improving iofile.py module.

--- untwisted/iofile.py
+++ untwisted/iofile.py
@@ -2,15 +2,16 @@ from untwisted.network import core, xmap, READ, WRITE, spawn, zmap
 from untwisted import iostd
 from untwisted.event import DUMPED, CLOSE, LOAD
 from collections import deque
+import os
 
 class DumpStr(iostd.DumpStr):
     def process(self, dev):
         try:
-            dev.write(self.data)  
-        except IOError as excpt:
-            spawn(dev, CLOSE, excpt)
+            size = os.write(dev.fd, self.data)  
+        except OSError as excpt:
+            self.process_error(dev, excpt.args[0])
         else:
-            self.data = buffer('')
+            self.data = buffer(self.data, size)
 
 class DumpFile(DumpStr, iostd.DumpFile):
     pass
@@ -30,12 +31,15 @@ class Stdout(iostd.Stdout):
     def update(self, dev):
         try:
             self.process_data(dev)
-        except IOError as excpt:
-            spawn(dev, CLOSE, excpt)
+        except OSError as excpt:
+            self.process_error(dev, excpt.args[0])
 
     def process_data(self, dev):
-        data = dev.read(self.SIZE)
-        spawn(dev, LOAD, data)
+        data = os.read(dev.fd, self.SIZE)
+        if not data: 
+            spawn(dev, CLOSE, '') 
+        else: 
+            spawn(dev, LOAD, data)
 
 def lose(device):
     """
