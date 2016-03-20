e9c7eb5e52b69c7fdbb59feae71abb3e58ca91e5
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-03-20 18:14:47 -0400

    Improving example.

--- demo/extra/echo_server.py
+++ demo/extra/echo_server.py
@@ -1,36 +1,26 @@
 from untwisted.network import Spin, xmap, core
-from untwisted.iostd import Server, Stdout, Stdin, ACCEPT, LOAD, CLOSE
+from untwisted.iostd import Server, Stdout, Stdin, ACCEPT, LOAD, CLOSE, lose
 
 class EchoServer(object):
-    def __init__(self, spin):
-        xmap(spin, ACCEPT, self.handle_accept)
+    def __init__(self):
+        server = Spin()
+        server.bind(('', 1234))
+        server.listen(200)
+    
+        Server(server)
+        xmap(server, ACCEPT, self.handle_accept)
 
-    def handle_accept(self, server, client):
-        Stdin(client)
-
-        Stdout(client)
+    def handle_accept(self, server, con):
+        Stdin(con)
+        Stdout(con)
        
-        xmap(client, LOAD, self.handle_load)
-        xmap(client, CLOSE, self.handle_close)
-
-    def handle_load(self, client, data):
-        client.dump(data)
-
-    def handle_close(self, client, err):
-        client.destroy()
-        client.close()
+        xmap(con, LOAD,  lambda con, data: con.dump(data))
+        xmap(con, CLOSE, lambda con, err: lose(con))
 
 if __name__ == '__main__':
-    spin = Spin()
-    spin.bind(('', 1234))
-    spin.listen(200)
-
-
-    Server(spin)
-
-
-    EchoServer(spin)
+    EchoServer()
     core.gear.mainloop()
+
 
 
 
