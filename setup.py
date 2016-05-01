0d782c88671bcd4e44c76d12abc5bf9695b46ccc
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-30 21:02:16 -0400

    Fixing setup version.

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@
 
 from distutils.core import setup
 setup(name="untwisted",
-      version="0.4",
+      version="0.0.1",
       packages=["untwisted", 
                 "untwisted.utils",
                 "untwisted.plugins"],
       scripts=['quickserv'],
       author="Iury O. G. Figueiredo",
-      author_email="ioliveira@id.uff.br")
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/untwisted',
+      download_url='https://github.com/iogf/untwisted',
+      keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 'framework', 'networking', 'protocols'],
+      classifiers=[],
+      description="A library for asynchronous programming in python.")
+
 
 
 
