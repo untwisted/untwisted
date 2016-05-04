514e8fac23f102a3e5efdbd517bfc8a709437101
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-05-04 13:00:42 -0400

    Fixing setup.py.

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ setup(name="untwisted",
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
       url='https://github.com/iogf/untwisted',
-      download_url='https://github.com/iogf/untwisted',
-      keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 'framework', 'networking', 'protocols'],
+      download_url='https://github.com/iogf/untwisted/releases',
+      keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 'framework', 'networking', 'protocols', 'rapidserv', 'irc', 'requests'],
       classifiers=[],
       description="A library for asynchronous programming in python.",)
+
+
 
 
 
