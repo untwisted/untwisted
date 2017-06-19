39bb540eb3114b5c21b997a2132f0240df4b224b
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-06-19 09:35:37 -0300

    Fixing setup.py version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name="untwisted",
-      version="1.1.0",
+      version="1.2.0",
       packages=["untwisted"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
@@ -11,6 +11,7 @@ setup(name="untwisted",
       keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 'framework', 'networking', 'protocols', 'rapidserv', 'irc', 'requests'],
       classifiers=[],
       description="A library for asynchronous programming in python.",)
+
 
 
 
