c5f2b4581ee0e0a6ffa018869590deb859ce6503
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-07-04 13:10:50 -0300

    Adding debugging tools.

--- untwisted/debug.py
+++ untwisted/debug.py
@@ -1,10 +1,17 @@
 from traceback import print_exc 
 import pprint
 
+def on_all(*args):
+    pprint.pprint(args)
+
+def on_event(con, *args):
+    pprint.pprint(args)
+
 def debug(event, params):
     print 'Event:%s' % event
     print 'Args:%s' % str(params)
     print_exc()
+
 
 
 
