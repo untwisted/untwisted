f8de0b3175d70b0261c1952cd6a83c63a3182432
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-06-26 17:14:13 -0300

    Improving tkinter integration module.

--- untwisted/tkinter.py
+++ untwisted/tkinter.py
@@ -1,17 +1,38 @@
 from untwisted.network import core
 
-def extern(obj, timeout=200):
-    core.gear.timeout = 0
+installed = False
 
+def install_hook(obj, timeout):
+    global installed
+
+    installed = True
+    core.gear.timeout = 0
     def loop():
         obj.after(timeout, loop)               
         core.gear.update()
-    
     obj.after(timeout, loop)               
 
+def extern(obj, timeout=200):
+    """
+    Tell Tkinter to process untnwisted event loop.
+    It registers just once the update handle.
+    """
+    global installed
+
+    # Register it just once.
+    if not installed: 
+        install_hook(obj, timeout)
+    installed = True
+
 def intern(obj, timeout):
+    """
+    Tell untwisted to process an extern event
+    loop.
+    """
+
     core.gear.timeout = timeout
     core.gear.pool.append(obj)
+
 
 
 
