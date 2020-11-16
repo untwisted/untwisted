da718a15b7a2bf28966570018571d054c7729d19
Iury O. G. Figueiredo <last.src@gmail.com>
2020-11-16 15:02:23 -0300

    Updating.

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@
 
 from distutils.core import setup
 setup(name="untwisted",
-      version="3.0.0",
+      version="2.0.3",
       packages=["untwisted"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
       url='https://github.com/iogf/untwisted',
       download_url='https://github.com/iogf/untwisted/releases',
-      keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 'framework', 'networking', 'protocols', 'rapidserv', 'irc', 'requests'],
+      keywords=['asynchronous', 'programming', 'twisted', 'untwisted', 'library', 
+      'framework', 'networking', 'protocols', 'rapidserv', 'irc', 'requests'],
       classifiers=[],
-      description="A library for asynchronous programming in python.",)
+      description="A better hell of callbacks for asynchronous programming. Untwisted is The Event Driven Framework for Python.",)
 
 
 
