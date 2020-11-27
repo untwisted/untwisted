a19d6c6c363364885cdbccec417657dd15b09030
Iury O. G. Figueiredo <last.src@gmail.com>
2020-11-27 04:43:34 -0300

    Fixing.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name="untwisted",
-      version="3.0.0",
+      version="3.0.1",
       packages=["untwisted"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
